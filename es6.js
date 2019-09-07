9d9b631a0a8fb3a4742f4ba38c1bf3169ee303e1
Wahed08 <wahedfarhad123@gmail.com>
2019-09-08 00:21:45 +0600

    package.json

---
+++ es6.js
@@ -0,0 +1,13 @@
+try {
+    var k = new Map();
+    console.log("ES6 supported!!")
+  } catch(err) {
+    console.log("ES6 not supported :(")
+  }
+  
+  try {
+    var k = new HashMap();
+    console.log("ES100 supported!!")
+  } catch(err) {
+    console.log("ES100 not supported :(")
+  }
